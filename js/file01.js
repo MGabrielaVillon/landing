ce49b1a8008297d62d209837bbbe9df3ccf1b4f3
MGabrielaVillon <117649344+MGabrielaVillon@users.noreply.github.com>
2026-06-08 19:52:54 +0000

    clase 08 06 2026

---
+++ js/file01.js
@@ -0,0 +1,31 @@
+"use strict";
+
+
+
+
+
+
+const showToast = () => {
+    const toastElement = document.getElementById('toast-interactive');
+    if (toastElement) {
+        toastElement.classList.add('md:block');
+    }
+};
+
+
+
+const showVideo = () => {
+    const demoElement = document.getElementById('demo');
+    if (demoElement) {
+        demoElement.addEventListener('click', () => {
+            window.open('https://www.youtube.com', '_blank');
+        });
+    }
+};
+
+
+
+(() => {
+    showToast();
+    showVideo();
+})();
